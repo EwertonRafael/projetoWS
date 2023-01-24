aaa67847823b3681d3eb8281af1f09fbc4eadf32
Ewerton Rafael <ton_rafael@hotmail.com>
2023-01-24 16:49:21 -0300

    projeto criado

---
+++ src/main/java/com/educandoweb/projetoWebervice/ProjetoWeberviceApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.projetoWebervice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoWeberviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoWeberviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/educandoweb/projetoWebervice/ProjetoWeberviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.projetoWebervice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWeberviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
